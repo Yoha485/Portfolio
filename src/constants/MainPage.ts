1e2ee0d7023249ac7cb3d5bf47b4551277d6dda3
Morozov Anton <antonmorozoff64@gmail.com>
2022-10-24 20:24:32 +0200

    initial project component

--- src/constants/MainPage.ts
+++ src/constants/MainPage.ts
@@ -1,2 +1,48 @@
+export interface Project {
+  href: string;
+  img: string;
+  description: string;
+  website: string;
+  code: string;
+}
+
 export const mainPageTitle = 'Software, app\nand web developer';
 export const mainPageDescription = `Are you looking for a fresh web developer? Highly motivated\n and fast learner? I'm here for you! You can download my CV/\nResume by clicking on the button below.`;
+
+export const projects: Array<Project> = [
+  {
+    href: '#footer',
+    img: '',
+    description: 'Dolore duis qui aliquip consectetur ipsum ullamco ad ea veniam.',
+    website: '',
+    code: ''
+  },
+  {
+    href: '#footer',
+    img: '',
+    description: 'Dolor quis elit aliqua excepteur cillum eu excepteur fugiat officia amet sunt.',
+    website: '',
+    code: ''
+  },
+  {
+    href: '#footer',
+    img: '',
+    description: 'Adipisicing id dolore veniam pariatur in amet ullamco dolore ut aliqua officia aliqua.',
+    website: '',
+    code: ''
+  },
+  {
+    href: '#footer',
+    img: '',
+    description: 'Non anim amet id cillum quis.',
+    website: '',
+    code: ''
+  },
+  {
+    href: '#footer',
+    img: '',
+    description: 'Incididunt laborum consectetur ex velit commodo labore ex tempor.',
+    website: '',
+    code: ''
+  }
+];
