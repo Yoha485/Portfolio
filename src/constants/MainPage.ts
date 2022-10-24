0d21fead2fabcaf130048a94f3c5059dba242360
Morozov Anton <antonmorozoff64@gmail.com>
2022-10-24 19:08:26 +0200

    update: intro component

--- src/constants/MainPage.ts
+++ src/constants/MainPage.ts
@@ -1,2 +1,2 @@
 export const mainPageTitle = 'Software, app\nand web developer';
-export const mainPageDescription = `Are you looking for a fresh web developer? Highly motivated and fast learner? I'm here for you! You can download my CV/Resume by clicking on the button below.`;
+export const mainPageDescription = `Are you looking for a fresh web developer? Highly motivated\n and fast learner? I'm here for you! You can download my CV/\nResume by clicking on the button below.`;
