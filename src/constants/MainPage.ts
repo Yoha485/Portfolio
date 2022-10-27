144c75704878f1925531db5b5f2d291685ec7ab2
Anton Morozov <anton.morozov@multiparts.net>
2022-10-27 17:55:17 +0200

    update: code refactoring

--- src/constants/MainPage.ts
+++ src/constants/MainPage.ts
@@ -15,7 +15,7 @@ export interface Skill {
 }
 
 export const mainPageTitle = 'Software, app\nand web developer';
-export const mainPageDescription = `Are you looking for a fresh web developer? Highly motivated\n and fast learner? I'm here for you! You can download my CV/\nResume by clicking on the button below.`;
+export const mainPageDescription = `Are you looking for a fresh web developer? Highly motivated\n and fast learner? I'm here for you! You can download my CV/\nResume by clicking on the circle.`;
 
 export const skills: Array<Skill> = [
   {
